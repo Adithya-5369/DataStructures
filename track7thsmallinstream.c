d31e6bbe60a268364a54341d98d1232d0e3565dd
M ADITHYA SAI SRINIVAS <145254524+Adithya-5369@users.noreply.github.com>
2024-12-14 18:28:51 +0530

    Update track7thsmallinstream.c

--- track7thsmallinstream.c
+++ track7thsmallinstream.c
@@ -5,60 +5,57 @@ Construct a time and space efficient algorithm to continuously keep track of the
 #include <stdio.h>
 #include <stdlib.h>
 
-#define HEAP_SIZE 7
+#define HEAP_CAPACITY 7
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap_elements(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
-// Function to heapify the max heap
-void maxHeapify(int heap[], int i, int size) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+void heapify_down(int arr[], int idx, int size) {
+    int largest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
 
-    if (left < size && heap[left] > heap[largest])
+    if (left < size && arr[left] > arr[largest])
         largest = left;
-    if (right < size && heap[right] > heap[largest])
+    if (right < size && arr[right] > arr[largest])
         largest = right;
 
-    if (largest != i) {
-        swap(&heap[i], &heap[largest]);
-        maxHeapify(heap, largest, size);
+    if (largest != idx) {
+        swap_elements(&arr[idx], &arr[largest]);
+        heapify_down(arr, largest, size);
     }
 }
 
-// Insert an element into the max heap
-void insertMaxHeap(int heap[], int *size, int value) {
-    heap[(*size)++] = value; // Add at the end
-    int i = *size - 1;
+void insert_into_heap(int arr[], int *size, int value) {
+    arr[(*size)++] = value;
+    int idx = *size - 1;
 
-    while (i > 0 && heap[(i - 1) / 2] < heap[i]) {
-        swap(&heap[(i - 1) / 2], &heap[i]);
-        i = (i - 1) / 2;
+    while (idx > 0 && arr[(idx - 1) / 2] < arr[idx]) {
+        swap_elements(&arr[(idx - 1) / 2], &arr[idx]);
+        idx = (idx - 1) / 2;
     }
 }
 
-// Replace root of max heap with a new value
-void replaceRoot(int heap[], int value, int size) {
-    heap[0] = value;
-    maxHeapify(heap, 0, size);
+void replace_root(int arr[], int value, int size) {
+    arr[0] = value;
+    heapify_down(arr, 0, size);
 }
 
-void trackSeventhSmallest(int stream[], int n) {
-    int maxHeap[HEAP_SIZE]; 
-    int size = 0;
+void track_seventh_smallest(int stream[], int len) {
+    int maxHeap[HEAP_CAPACITY];
+    int heap_size = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (size < HEAP_SIZE) {
-            insertMaxHeap(maxHeap, &size, stream[i]);
+    for (int i = 0; i < len; i++) {
+        if (heap_size < HEAP_CAPACITY) {
+            insert_into_heap(maxHeap, &heap_size, stream[i]);
         } else if (stream[i] < maxHeap[0]) {
-            replaceRoot(maxHeap, stream[i], size);
+            replace_root(maxHeap, stream[i], heap_size);
         }
 
-        if (size == HEAP_SIZE) {
+        if (heap_size == HEAP_CAPACITY) {
             printf("7th smallest so far: %d\n", maxHeap[0]);
         } else {
             printf("Less than 7 elements seen so far\n");
@@ -70,6 +67,6 @@ int main() {
     int stream[] = {15, 2, 7, 20, 1, 5, 9, 3, 6, 8, 4, 12, 11, 10, 13};
     int n = sizeof(stream) / sizeof(stream[0]);
 
-    trackSeventhSmallest(stream, n);
+    track_seventh_smallest(stream, n);
     return 0;
 }
